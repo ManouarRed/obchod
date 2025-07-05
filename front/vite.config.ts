35bcf809883d84653c640636d4eb5318d770161f
ManouarRed <139481088+ManouarRed@users.noreply.github.com>
2025-07-05 18:29:15 +0200

    Update vite.config.ts

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -11,9 +11,8 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, path.resolve(__dirname, '..'), '');
 
   return {
+    
     define: {
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'import.meta.env.VITE_API_BASE_URL': JSON.stringify(env.VITE_API_BASE_URL)
     },
     resolve: {
